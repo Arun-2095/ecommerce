259ec2334fd63b8011725777b5b21ff3fcb458e4
arunkumar <arunagiri2195@gmail.com>
2021-07-02 20:23:48 +0530

    build setup & router setup

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,6 +8,11 @@ const routes: Routes = [
   pathMatch:'full'
 },
 {
+  path:'user',
+  redirectTo:'/user/dashboard',
+  pathMatch:'full'
+},
+{
   path:'auth',
   loadChildren:() => import('./auth/auth.module').then( m => m.AuthModule)
 },
